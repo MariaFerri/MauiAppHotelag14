eb8aa90278c2128a27a3e15870981219cde2d253
Maria Ferri <malu210797@gmail.com>
2025-05-25 23:08:58 -0300

    Primeiro commit do projeto MAUI

---
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiAppHotel.Views;
+
+public partial class HospedagemContratada : ContentPage
+{
+    public HospedagemContratada()
+    {
+        InitializeComponent();
+       
+    }
+
+    private async void OnVoltarClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+}
